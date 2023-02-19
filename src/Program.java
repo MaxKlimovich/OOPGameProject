ed69cbbf99da5249f7ba551f4c0c75a4663b7823
user <Klimovich.maks@gmail.com>
2023-02-19 22:13:42 +0300

    add Program

--- src/Program.java
+++ src/Program.java
@@ -1,49 +1,100 @@
-//package Salary;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Program {
+    public static void main(String[] args) {
+        int teamCount = 5;
+        Random rnd = new Random();
+
+        System.out.println("Team #1");
+        ArrayList<BaseHero> team = new ArrayList<>();
+        for (int i = 0; i < teamCount; i++) {
+            int val1 = rnd.nextInt(4);
+            switch (val1) {
+                case 0:
+                    team.add(new Monk());
+                    break;
+                case 1:
+                    team.add(new Peasant());
+                    break;
+                case 2:
+                    team.add(new Pickener());
+                    break;
+                case 3:
+                    team.add(new CrossBowMan());
+                    break;
+//                case 4:
+//                    team.add(new Sniper());
+//                    break;
+//                case 5:
+//                    team.add(new Rogue());
+//                    break;
+//                case 6:
+//                    team.add(new Warlock());
+//                    break;
+            }
+            System.out.println(team.get(i).getInfo());
+        }
+        System.out.println("\n------------------------------------------------------------\n");
+
+        System.out.println("Team #2 ");
+
+        ArrayList<BaseHero> team2 = new ArrayList<>();
+        for (int i = 0; i < teamCount; i++) {
+            int val2 = rnd.nextInt(4);
+            switch (val2) {
+                case 0:
+                    team2.add(new Sniper());
+                    break;
+                case 1:
+                    team2.add(new Warlock());
+                    break;
+                case 2:
+                    team2.add(new Rogue());
+                    break;
+                case 3:
+                    team2.add(new Peasant());
+                    break;
+//                case 4:
+//                    team2.add(new CrossBowMan());
+//                    break;
+//                case 5:
+//                    team2.add(new Monk());
+//                    break;
+//                case 6:
+//                    team2.add(new Pickener());
+//                    break;
+            }
+            System.out.println(team2.get(i).getInfo());
+        }
+
+        System.out.println("\n------------------------------------------------------------\n");
+
+        ArrayList<BaseHero> allArray = new ArrayList<>(team);
+        for (int i = 0; i < team.size(); i++) {
+            allArray.add(team2.get(i));
+        }
+
+        @Override
+        public String getString() {
+            return String.format(allArray);
+        }
+
+        System.out.println(allArray.toString());
+
+
+//        ArrayList allArray = new ArrayList<>(team);
+//        allArray.add(team2);
 //
-//import Salary.SalaryComporator;
-//
-//import java.util.*;
-//
-////import java.util.ArrayList;
-////import java.util.List;
-////import java.util.Random;
-////
-//public class Program {
-//    //    public static void main(String[] args) {
-////        int teamCount = 10;
-////        Random rnd = new Random();
-////
-////        List<BaseHero> teams = new ArrayList<>();
-////        for (int i = 0; i < teamCount; i++) {
-////            int val = rnd.nextInt(2);
-////            switch (val) {
-////                case 0:
-////                    teams.add(new Rogue());
-////                    break;
-////                case 1:
-////                    teams.add(new CrossBowMan());
-////                    break;
-////                case 2:
-////                    teams.add(new Monk());
-////                    break;
-////                case 3:
-////                    teams.add(new Peasant());
-////                    break;
-////                case 4:
-////                    teams.add(new Pikeman());
-////                    break;
-////                case 5:
-////                    teams.add(new Sniper());
-////                    break;
-////                case 6:
-////                    teams.add(new Warlock());
-////                    break;
-////            }
-////            System.out.println(teams.get(i).getInfo());
-////        }
-////        System.out.println();
-////    }
-////}
+//        System.out.println(allArray);
+
+    }
+
+
+}
+
+
 //    public static void main(String[] args) {
 ////        List<Integer> nums = new ArrayList<>();
 ////        nums.add(1);
