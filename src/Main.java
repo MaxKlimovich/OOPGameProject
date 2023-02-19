95b3ae6396c253ce114221a31bff60872d1ca059
user <Klimovich.maks@gmail.com>
2023-02-19 22:13:21 +0300

    add Main

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,63 @@
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Random;
+//
 public class Main {
     public static void main(String[] args) {
-        Peasant hero1 = new Peasant();
-        System.out.println(hero1.getInfo());
 
-        Rogue hero2 = new Rogue();
-        System.out.println(hero2.getInfo());
 
-        Sniper hero3 = new Sniper();
-        System.out.println(hero3.getInfo());
-
-        Warlock hero4 = new Warlock();
-        System.out.println(hero4.getInfo());
-
-        Pickener hero5 = new Pickener();
-        System.out.println(hero5.getInfo());
-
-        CrossBowMan hero6 = new CrossBowMan();
-        System.out.println(hero6.getInfo());
-
-        Monk hero7 = new Monk();
-        System.out.println(hero7.getInfo());
+        }
+//        Random r = new Random();
+//
+//        Peasant hero1 = new Peasant();
+//        System.out.println(hero1.getInfo());
+//
+//        Rogue hero2 = new Rogue();
+//        System.out.println(hero2.getInfo());
+//
+//        Sniper hero3 = new Sniper();
+//        System.out.println(hero3.getInfo());
+//
+//        Warlock hero4 = new Warlock();
+//        System.out.println(hero4.getInfo());
+//
+//        Pickener hero5 = new Pickener();
+//        System.out.println(hero5.getInfo());
+//
+//        CrossBowMan hero6 = new CrossBowMan();
+//        System.out.println(hero6.getInfo());
+//
+//        Monk hero7 = new Monk();
+//        System.out.println(hero7.getInfo());
+//
+//
+//    }
+//}
 
 //        for (int i = 0; i < 10; i++) {
 //            System.out.println(getName());
 //        }
 
-    }
-    private static String getName() {
-        String name = String.valueOf(Names.values()[new Random().nextInt(Names.values().length-1)]);
-        return name;
+//        ArrayList<Teams> db = new ArrayList<>();
+//        for (int i = 0; i < 5; i++) {
+//            db.add(new Teams("name" +i, "hp" +i, "speed" +i));
+//        }
+//        System.out.println(db);
+////        db.sort(new SalaryComporator());
+//
+//        System.out.println(db);
+
+//    }
+//    public static String getName() {
+//        String name = String.valueOf(Names.values()[new Random().nextInt(Names.values().length-1)]);
+//        return name;
     }
 
-}
+
+
+
+
+
--- src/Teams.java
+++ src/Teams.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class Teams {
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class Teams implements Comparable<Teams> {
+    public String name;
+    public int hp;
+    public int speed;
+
+
+    public Teams( String name, int hp, int speed) {
+        this.name = name;
+        this.hp = hp;
+        this.speed = speed;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("\n%s, %d, %d", name, hp, speed);
+    }
+
+    @Override
+    public int compareTo(Teams o) {
+        return Integer.compare(this.speed, o.speed);
+//        if (this.speed > o.speed)
+//            return 1;
+//        else if (this.speed < o.speed)
+//            return 1;
+//        else
+//            return 0;
+    }
 }
+//    int index;
+//
+//        @Override
+//        public boolean hasNext() {
+//            return index++ <4;
+//        }
+//
+//    @Override
+//    public String next() {
+//        switch (index) {
+//            case 1:
+//                return String.format("firstName: %s", firstName);
+//            case 2:
+//                return String.format("SecondName: %s",secondName);
+//            case 3:
+//                return String.format("age: %s", 24);
+//            default:
+//                return String.format("Salery: %s", 4232);
+//        }
+//    }
+
+
+
