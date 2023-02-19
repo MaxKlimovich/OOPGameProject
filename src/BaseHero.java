0925fe51df22b506986edd3496728bbc42fffa44
user <Klimovich.maks@gmail.com>
2023-02-19 22:07:45 +0300

    add Base Hero)

--- src/BaseHero.java
+++ src/BaseHero.java
@@ -10,31 +10,49 @@ public abstract class BaseHero implements GameInterface{
     protected String name;
     private  int hp;
     protected int maxHp;
+    protected int attack;
+    protected int damageMin;
+    protected int damageMax;
+    protected int defense;
     protected int speed;
-    protected int maxSpeed;
+    protected int coordinataX;
+
+    protected int coordinataY;
 
 
     static  {
         BaseHero.number = 0;
         BaseHero.r = new Random();
     }
-    public BaseHero(String name, int hp, int maxHp, int speed) {
+    public BaseHero(String name, int hp, int maxHp, int attack, int damageMin, int damageMax,
+                    int defense,  int speed, int coordinataX, int coordinataY) {
         this.name = name;
         this.hp = hp;
         this.maxHp = maxHp;
+        this.attack = attack;
+        this.defense = defense;
+        this.damageMin = damageMin;
+        this.damageMax = damageMax;
         this.speed = speed;
-        this.maxSpeed = speed;
-
+        this.coordinataX = coordinataX;
+        this.coordinataY = coordinataY;
 
     }
     public BaseHero() {
-        this(String.format("Hero_BaseHero #%d", ++BaseHero.number),
-                BaseHero.r.nextInt(100, 200), 200,
-                BaseHero.r.nextInt(50, 150));
+        this(String.format("Hero_BaseHero #%d", ++BaseHero.number), /** Name */
+                BaseHero.r.nextInt(1, 30), 30, /** HP and MaxHP */
+                BaseHero.r.nextInt(1, 17), /** Attack */
+                 10,10, /** Damage Min & Max */
+                BaseHero.r.nextInt(1,12), /** Defense */
+                BaseHero.r.nextInt(3, 9), /** Speed */
+                12, 14); /** Coordinate X & Y */
+
     }
     public String getInfo() {
-        return String.format ("Name: %s, Hp: %d, MaxHp: %d, Speed: %s, Type: %s",
-                this.name, this.hp, this.maxHp, this.speed, this.getClass().getSimpleName());
+        return String.format ("Name: %s, Hp: %d, MaxHp: %d, Attack: %d, DamageMin: %d, DamageMax: %d," +
+                        " Defense: %d, Speed: %d, X: %d, Y: %d, Type: %s",
+                this.name, this.hp, this.maxHp, this.attack, this.damageMin, this.damageMax, this.defense,
+                this.speed, this.coordinataX, this.coordinataY, this.getClass().getSimpleName());
     }
     public void healer(int Hp) {
         this.hp = Hp + this.hp > this.maxHp ? this.maxHp : Hp + this.hp;
@@ -53,6 +71,8 @@ public abstract class BaseHero implements GameInterface{
     }
     @Override
     public String getMessage() {return "I am I";}
+
+
 }
 
 
--- src/Program.java
+++ src/Program.java
@@ -1,40 +1,89 @@
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Random;
+//package Salary;
 //
+//import Salary.SalaryComporator;
+//
+//import java.util.*;
+//
+////import java.util.ArrayList;
+////import java.util.List;
+////import java.util.Random;
+////
 //public class Program {
+//    //    public static void main(String[] args) {
+////        int teamCount = 10;
+////        Random rnd = new Random();
+////
+////        List<BaseHero> teams = new ArrayList<>();
+////        for (int i = 0; i < teamCount; i++) {
+////            int val = rnd.nextInt(2);
+////            switch (val) {
+////                case 0:
+////                    teams.add(new Rogue());
+////                    break;
+////                case 1:
+////                    teams.add(new CrossBowMan());
+////                    break;
+////                case 2:
+////                    teams.add(new Monk());
+////                    break;
+////                case 3:
+////                    teams.add(new Peasant());
+////                    break;
+////                case 4:
+////                    teams.add(new Pikeman());
+////                    break;
+////                case 5:
+////                    teams.add(new Sniper());
+////                    break;
+////                case 6:
+////                    teams.add(new Warlock());
+////                    break;
+////            }
+////            System.out.println(teams.get(i).getInfo());
+////        }
+////        System.out.println();
+////    }
+////}
 //    public static void main(String[] args) {
-//        int teamCount = 10;
-//        Random rnd = new Random();
+////        List<Integer> nums = new ArrayList<>();
+////        nums.add(1);
+////        nums.add(123);
+////        nums.add(1234);
+////        nums.add(12345);
+////        nums.add(123456);
 //
-//        List<BaseHero> teams = new ArrayList<>();
-//        for (int i = 0; i < teamCount; i++) {
-//            int val = rnd.nextInt(2);
-//            switch (val) {
-//                case 0:
-//                    teams.add(new Rogue());
-//                    break;
-//                case 1:
-//                    teams.add(new CrossBowMan());
-//                    break;
-//                case 2:
-//                    teams.add(new Monk());
-//                    break;
-//                case 3:
-//                    teams.add(new Peasant());
-//                    break;
-//                case 4:
-//                    teams.add(new Pikeman());
-//                    break;
-//                case 5:
-//                    teams.add(new Sniper());
-//                    break;
-//                case 6:
-//                    teams.add(new Warlock());
-//                    break;
-//            }
-//            System.out.println(teams.get(i).getInfo());
+//
+//        Random r = new Random();
+////        List<Integer> numbers= new ArrayList<>();
+////        for (int i = 0; i < 10; i++) {
+////            numbers.add(r.nextInt(1, 10));
+////        }
+////        System.out.println(numbers);
+////        Collections.sort(numbers);
+////        System.out.println(numbers);
+//
+//
+//
+//        List<Teams> db = new ArrayList<>();
+//        for (int i = 0; i < 5; i++) {
+//            db.add(new Teams("name" +i, "second name" +i,
+//                    r.nextInt(18, 35), r.nextInt(10000)));
 //        }
-//        System.out.println();
+//        System.out.println(db);
+//        db.sort(new SalaryComporator());
+////        Collections.sort(db);
+//        System.out.println(db);
+//
+//
+//
+////        Teams teams = new Teams(
+////                "firstName", "secondName", 24, 1222 );
+////
+////        Iterator<String> components = teams;
+////        while (components.hasNext()){
+////            System.out.println(teams.next());
+////        }
+//
+//
 //    }
 //}
