f417bb9423eea03ff5755ff59489fd86e0180da0
user <Klimovich.maks@gmail.com>
2023-02-23 18:48:24 +0300

    add Vector 2D 23.02

--- src/Vector2D.java
+++ src/Vector2D.java
@@ -1,2 +1,14 @@
-public class Coordinate {
+public class Vector2D {
+    public int x;
+    public int y;
+
+    public Vector2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public static int getDistance(BaseHero h1, BaseHero h2){
+        return (int) Math.sqrt((h1.pos.x - h2.pos.x)^2 + (h1.pos.y - h2.pos.y)^2);
+    }
 }
