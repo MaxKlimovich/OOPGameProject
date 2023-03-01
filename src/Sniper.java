8ba62041bbd1b7dbbef23f503daf16f321fe4828
user <Klimovich.maks@gmail.com>
2023-03-01 17:33:22 +0300

    add Sniper 01.03

--- src/Sniper.java
+++ src/Sniper.java
@@ -1,36 +1,23 @@
+import java.util.ArrayList;
+
 public class Sniper extends DistantBattle {
-
-    private int energy;
-    private int maxEnergy;
-    private String weapon;
-
-
     public Sniper(String name, int x, int y) {
-        super(String.format("Sniper №%d", ++Sniper.number),
-                BaseHero.r.nextInt(1, 15), 15,
-                12, 8, 10, 10,
-                9, x, y, 32, 32, 400);
-        this.maxEnergy = 100; /** Energy */
-        this.energy = Sniper.r.nextInt(0, 100);
-        this.weapon = String.format("Gun "); /** Weapon type */
-        this.getMessage();
-    }
-
-    public int Attack() {
-        int damage = Sniper.r.nextInt(20, 30);
-        this.energy -= (int) (damage * 0.8);
-        if (energy < 0) return 0;
-        else return damage;
-    }
-
-    public String getInfo() {
-        return String.format("%s, Energy: %d, Weapon: %s, Shot: %d, MaxShot: %d, Distance: %d, Message: %s.",
-                super.getInfo(), this.energy, this.weapon, super.cartridges, super.maxShot,
-                super.distance, this.getMessage());
+        super(name, 9, 15, 10, 8, 10, x, y, 12,10);
     }
 
     @Override
-    public void die() {
-        System.out.println("Keeeeek");
+    public void step(ArrayList<BaseHero> team1, ArrayList<BaseHero> team2) {
+        super.step(team1, team2);
+    }
+    @Override
+    public String toString() {
+        return String.format("Sniper: %s | Hp: %d | Speed: %d | ATK: %d-%d | DEF: %d | (X,Y): (%d,%d) | Status: %s | " +
+                        "Cartriges: %d\n",
+                this.name, this.hp, this.speed, this.damageMin, this.damageMax, this.defense,
+                this.pos.x, this.pos.y, this.state, this.cartridges);
+    }
+    public String getInfo() {
+        return "Sniper";
     }
 }
+
