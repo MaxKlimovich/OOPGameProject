831a4daea02b1ab344c6c3584d83dbedad550a63
user <Klimovich.maks@gmail.com>
2023-03-01 17:35:20 +0300

    add Distant Battle 01.03

--- src/DistantBattle.java
+++ src/DistantBattle.java
@@ -2,57 +2,36 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public abstract class DistantBattle extends BaseHero {
-    protected int maxShot;
-    int cartridges;
-    int maxCartridges;
-    int distance;
 
-    public DistantBattle(String name, int hp, int maxHp, int attack, int damageMin,
-                         int damageMax, int defense, int speed, int coordinateX, int coordinateY,
-                         int cartridges, int maxCartridges, int distance) {
-        super(name, hp, maxHp, attack, damageMin, damageMax, defense, speed, coordinateX, coordinateY);
+    int cartridges;
+
+
+
+    public DistantBattle(String name, int speed, int maxHp, int defense, int damageMin, int damageMax,
+                         int x, int y, int attack, int cartridges) {
+        super(name, speed, maxHp, defense, damageMin, damageMax, x, y, attack);
         this.cartridges = cartridges;
-        this.maxCartridges = maxCartridges;
-        this.distance = distance;
+
     }
 
     @Override
     public void step(ArrayList<BaseHero> team1, ArrayList<BaseHero> team2) {
-        Random rnd = new Random();
-//        int index = super.findNearest(team2);
-        if (team1.contains(this)) { //Проверка если он находится в 1 команде
-            int min = 20;
-            int tmp;
-            int index = 0;
-            for (int i = 0; i < team2.size(); i++) { //Ищем дистанцию со второй команде
-                tmp = Vector2D.getDistance(this, (BaseHero) team2.get(i)); //Расстояние до врага
-                if (min > tmp) {
-                    min = tmp; //записываем значение минимальной дистанции.
-                    index = i; //запоминаем индекс ближайшего врага.
-                }
+        if (state.equals("Die") || cartridges == 0) return;
+        int target = findNearest(team2);
+        float damage = (team2.get(target).defense - attack > 0) ?
+                damageMin : (team2.get(target).defense - attack < 0) ?
+                damageMax : (damageMax + damageMin) / 2;
+        team2.get(target).getDamage(damage);
+
+        for (int i = 0; i < team1.size(); i++) {
+//            if (team1.get(i).getInfo().toString().split(":")[0].equals("Peasant" &&
+//            team1.get(i).state.equals("Stand")) { //Можно писать и вот так
+            if (team1.get(i) instanceof Fermer && team1.get(i).state.equals("Stand ")) {
+                System.out.println("Fermer " + team1.get(i).name + "Busy ");
+                team1.get(i).state = "Busy ";
+                return;
             }
-            System.out.println(team2.get(index));
-            team2.get(index).hp = team2.get(index).hp - rnd.nextInt(this.damageMin, this.damageMax); //Выстрел в персонажа
-            System.out.println(team2.get(index));
-         } else { // Он будет находиться во второй команде
-            int min = 0;
-            int tmp;
-            int index = 0;
-            for (int i = 0; i < team1.size(); i++) { //пробегаемся по первой команде для поиска дистанции
-                tmp = Vector2D.getDistance(this, (BaseHero) team1.get(i));
-                if (min > tmp) {
-                    min = tmp; //записываем значение минимальной дистанции.
-                    index = i; //запоминаем индекс ближайшего врага.
-                }
-            }
-            System.out.println(team1.get(index));
-            team1.get(index).hp = team1.get(index).hp - rnd.nextInt(this.damageMin, this.damageMax); //Выстрел в персонажа
-            System.out.println(team1.get(index));
         }
+        cartridges--;
     }
-
-//    public float getDistant() {return distance;}
-
-    @Override
-    public String getMessage() {return "I'm the longest shooting smug fucker";}
 }
