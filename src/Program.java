0b833c31437f34e770f1dbc636e5406c2b0c6682
user <Klimovich.maks@gmail.com>
2023-03-01 17:36:50 +0300

    add allllllll 01.03

--- src/Program.java
+++ src/Program.java
@@ -1,97 +1,66 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Random;
-
-public class Program {
-    public static void main(String[] args) {
-        int teamCount = 5;
-        Random rnd = new Random();
-
-        System.out.println("Team #1");
-        int x1 = 1;
-        int y1 = 0;
-        ArrayList<BaseHero> team1 = new ArrayList<>();
-        for (int i = 0; i < teamCount; i++) {
-            y1++;
-            int val1 = rnd.nextInt(4);
-            switch (val1) {
-                case 0:
-                    team1.add(new Monk("Monk", x1, y1));
-                    break;
-                case 1:
-                    team1.add(new Peasant("Peasant", x1, y1));
-                    break;
-                case 2:
-                    team1.add(new Pickener("Pickener", x1, y1));
-                    break;
-                case 3:
-                    team1.add(new CrossBowMan("BowMan", x1, y1));
-                    break;
+//import java.util.*;
+//
+//public class Program {
+//    static final int teamCount = 10;
+//
+//    public static void main(String[] args) {
+//
+//        ArrayList<BaseHero> team1 = new ArrayList<>();
+//        ArrayList<BaseHero> team2 = new ArrayList<>();
+//        ArrayList<BaseHero> allTeam = new ArrayList<>();
+//        Scanner user_input = new Scanner(System.in);
+//        createTeam(team1, 0, 1);
+//        createTeam(team2, 3, 10);
+//        allTeam.addAll(team1);
+//        allTeam.addAll(team2);
+//        sortTeam(allTeam);
+//
+//        System.out.println(allTeam);
+//        while (true) {
+//            for (BaseHero baseHero : allTeam) {
+//                if (team1.contains(baseHero)) baseHero.step(team1, team2);
+//                else baseHero.step(team2, team1);
+//            }
+//            user_input.nextLine();
+//            System.out.println(allTeam);
+//        }
+//    }
+//    static void createTeam(ArrayList team, int offset, int posY) {
+//        for (int i = 0; i <= teamCount; i++) {
+//            int rnd = new Random().nextInt(4) + offset;
+//            switch (rnd) {
+//                case 0:
+//                    team.add(new Monk (BaseHero.getName(), i + 1, posY));
+//                    break;
+//                case 1:
+//                    team.add(new Fermer (BaseHero.getName(), i , posY));
+//                    break;
+//                case 2:
+//                    team.add(new Pickener(BaseHero.getName(), i , posY));
+//                    break;
+//                case 3:
+//                    team.add(new BowMan(BaseHero.getName(), i , posY));
+//                    break;
 //                case 4:
-//                    team1.add(new Sniper());
+//                    team.add(new Sniper(BaseHero.getName(), i , posY));
 //                    break;
 //                case 5:
-//                    team1.add(new Rogue());
+//                    team.add(new Rogue(BaseHero.getName(), i , posY));
 //                    break;
 //                case 6:
-//                    team1.add(new Warlock());
+//                    team.add(new Warlock(BaseHero.getName(), i , posY));
 //                    break;
-            }
-            System.out.println(team1.get(i).getInfo());
-        }
-        System.out.println("\n------------------------------------------------------------\n");
-
-
-        System.out.println("Team #2 ");
-        int x2 = 10;
-        int y2 = 0;
-        ArrayList<BaseHero> team2 = new ArrayList<>();
-        for (int i = 0; i < teamCount; i++) {
-            y2++;
-            int val2 = rnd.nextInt(4);
-            switch (val2) {
-                case 0:
-                    team2.add(new Sniper("Sniper", x2, y2));
-                    break;
-                case 1:
-                    team2.add(new Warlock("Warlock", x2, y2));
-                    break;
-                case 2:
-                    team2.add(new Rogue("Rogue", x2, y2));
-                    break;
-                case 3:
-                    team2.add(new Peasant("Peasant", x2, y2));
-                    break;
-//                case 4:
-//                    team2.add(new CrossBowMan());
-//                    break;
-//                case 5:
-//                    team2.add(new Monk());
-//                    break;
-//                case 6:
-//                    team2.add(new Pickener());
-//                    break;
-            }
-            System.out.println(team2.get(i).getInfo());
-        }
-
-        System.out.println("\n------------------------------------------------------------\n");
-
-        System.out.println("Sorted team1 & team2: ");
-        ArrayList<BaseHero> allArray = new ArrayList<>(team1);
-        for (int i = 0; i < team1.size(); i++) {
-            allArray.add(team2.get(i));
-        }
-        allArray.sort(new Comparator<BaseHero>() {
-            @Override
-            public int compare(BaseHero o1, BaseHero o2) {
-                if (o2.getSpeed() == o1.getSpeed()) {
-                    return o2.hp - o1.hp;
-                }
-                return o2.getSpeed() - o1.getSpeed();
-            }
-        });
-        System.out.println(allArray.toString());
-    }
-}
+//            }
+//        }
+//    }
+//
+//    static void sortTeam(ArrayList<BaseHero> team) {
+//        team.sort(new Comparator<BaseHero>() {
+//            @Override
+//            public int compare(BaseHero o1, BaseHero o2) {
+//                if (o2.getSpeed() == o1.getSpeed()) return (int) (o2.hp - o1.hp);
+//                else return (int) (o2.getSpeed() - o1.getSpeed());
+//            }
+//        });
+//    }
+//}
