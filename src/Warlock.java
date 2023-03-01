4937bfd1cb7dfb1a05331fda1faf694a60c50c42
user <Klimovich.maks@gmail.com>
2023-03-01 17:32:31 +0300

    add Warlock 01.03

--- src/Warlock.java
+++ src/Warlock.java
@@ -1,39 +1,24 @@
+import java.util.ArrayList;
+
 public class Warlock extends BaseHero {
 
-    private int mana;
-    private int maxMana;
-    private String weapon;
 
+        public Warlock(String name, int x, int y) {
+            super(name, 9, 30, 12, -5, -5, x, y, 17);
+        }
+        @Override
+        public void step(ArrayList<BaseHero> team1, ArrayList<BaseHero> team2) {
+            super.step(team1, team2);
+        }
+        @Override
+        public String toString() {
+            return String.format("BowMan: %s | Hp: %d |  Speed: %d | ATK: %d-%d  |  DEF: %d  " +
+                            "|  (X,Y): (%d,%d)  | Status: %s\n",
+                    this.name, this.hp, this.speed, this.damageMin, this.damageMax,
+                    this.defense, this.pos.x, this.pos.y, this.state);
+        }
 
-    public Warlock(String name, int x, int y) {
-        super(String.format("Warlock №%d", ++Warlock.number),
-                BaseHero.r.nextInt(1, 30), 30,
-                17, -5, -5, 12, 9, x,y);
-        this.maxMana = 300;
-        this.mana = Warlock.r.nextInt(250, 300);
-        this.weapon = String.format("Staff "); /** Weapon type */
-        this.getMessage();
-    }
-
-    public int Attack() {
-        int damage = Warlock.r.nextInt(20, 30);
-        this.mana -= (int) (damage * 0.8);
-        if (mana < 0) return 0;
-        else return damage;
-    }
-
-    public String getInfo() {
-        return String.format("%s, MaxMana: %d, Mana: %d, Weapon: %s, Message: %s", super.getInfo(),
-                this.maxMana, this.mana, this.weapon, this.getMessage());
-    }
-
-    @Override
-    public void die() {
-        System.out.println("Diiiiieeeee");
-    }
-
-    @Override
-    public String getMessage() {
-        return "i will fix your head";
-    }
+        public String getInfo() {
+            return "Warlock";
+        }
 }
