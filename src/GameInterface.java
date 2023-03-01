adc95421a87a8a4b6d5f523d7b16af135677b99b
user <Klimovich.maks@gmail.com>
2023-03-01 17:34:51 +0300

    add GameInterface 01.03

--- src/GameInterface.java
+++ src/GameInterface.java
@@ -3,7 +3,5 @@ import java.util.ArrayList;
 public interface GameInterface {
 
     default void step(ArrayList<BaseHero> team, ArrayList<BaseHero>team2){}
-    String getMessage();
-
-
+    String getInfo();
 }
