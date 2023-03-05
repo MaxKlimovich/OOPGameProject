5a7694f00c174a6c90af8d8f7f38d6f7b7c4270d
user <Klimovich.maks@gmail.com>
2023-03-05 15:09:18 +0300

    add main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import BaseHero.*;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Random;
@@ -6,27 +8,27 @@ import java.util.Scanner;
 public class Main {
 
     static final int teamCount = 10;
+    public static ArrayList<BaseHero> allTeam = new ArrayList<>();
+    public static ArrayList<BaseHero> team1 = new ArrayList<>();
+    public static ArrayList<BaseHero> team2 = new ArrayList<>();
 
     public static void main(String[] args) {
-
-        ArrayList<BaseHero> team1 = new ArrayList<>();
-        ArrayList<BaseHero> team2 = new ArrayList<>();
-        ArrayList<BaseHero> allTeam = new ArrayList<>();
         Scanner user_input = new Scanner(System.in);
+        System.out.print("Press Enter to begin.");
+        user_input.nextLine();
         createTeam(team1, 0, 1);
         createTeam(team2, 3, 10);
         allTeam.addAll(team1);
         allTeam.addAll(team2);
         sortTeam(allTeam);
 
-        System.out.println(allTeam);
         while (true) {
+            View.view();
+            user_input.nextLine();
             for (BaseHero baseHero : allTeam) {
                 if (team1.contains(baseHero)) baseHero.step(team1, team2);
                 else baseHero.step(team2, team1);
             }
-            user_input.nextLine();
-            System.out.println(allTeam);
         }
     }
     static void createTeam(ArrayList team, int offset, int posY) {
@@ -34,10 +36,10 @@ public class Main {
             int rnd = new Random().nextInt(4) + offset;
             switch (rnd) {
                 case 0:
-                    team.add(new Monk (BaseHero.getName(), i + 1, posY));
+                    team.add(new Monk(BaseHero.getName(), i + 1, posY));
                     break;
                 case 1:
-                    team.add(new Fermer (BaseHero.getName(), i , posY));
+                    team.add(new Fermer(BaseHero.getName(), i , posY));
                     break;
                 case 2:
                     team.add(new Pickener(BaseHero.getName(), i , posY));
@@ -62,10 +64,14 @@ public class Main {
         team.sort(new Comparator<BaseHero>() {
             @Override
             public int compare(BaseHero o1, BaseHero o2) {
-                if (o2.getSpeed() == o1.getSpeed()) return (int) (o2.hp - o1.hp);
+                if (o2.getSpeed() == o1.getSpeed()) return (int) (o2.getHp() - o1.getHp());
                 else return (int) (o2.getSpeed() - o1.getSpeed());
             }
         });
+    }
+
+    static String getName() { // random name
+        return String.valueOf(Names.values()[new Random().nextInt(Names.values().length - 1)]);
     }
 }
 
